c9759b36ef71aa0a934f8ac444caca0d34e305a0
erlangga092 <erlanggaangga092@gmail.com>
2022-12-20 21:27:24 +0700

    Add public/build

---
+++ public/build/assets/index.9a14dc14.js
@@ -0,0 +1,1 @@
+import{R as e}from"./app.b568e1c6.js";const s=({title:a,icon:t})=>e.createElement("div",{className:"card-header"},e.createElement("span",{className:"font-weight-bold"},e.createElement("i",{className:t})," ",a));export{s as A};
